05df8c6922a05b8b32e82becca3c682179027724
Renzo Marquina 99 <116982250+RMA99@users.noreply.github.com>
2023-01-09 14:44:47 -0500

    segundo commit

---
+++ android/app/src/main/kotlin/com/codigorenzo/proy_final_renzo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.codigorenzo.proy_final_renzo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/application/use_cases/frmRegistroCompra.dart
+++ lib/application/use_cases/frmRegistroCompra.dart
@@ -67,7 +67,7 @@ class _FrmRegistroCompraState extends State<FrmRegistroCompra> {
                           });
                         },
                       ),
-                      Text("Logrado"),
+                      Text("Logrados"),
                     ],
                   ),
                 ],
